cbb3e1f8f5ecad4653e2fbfd96bcc8d0a8343fe0
Saulgp-daw <saulgp4a@gmail.com>
2023-09-21 16:18:49 +0100

    Proceso ping incluido

--- Procesos/src/org/example/Main.java
+++ Procesos/src/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -8,8 +9,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //crearProceso("code");
         //crearProceso("code");
-        imprimirFicheroConsola("/home/dam2/Descargas/test.txt");
-
+        //imprimirFicheroConsola("/home/dam2/Descargas/test.txt");
+        procesoPing("ping");
     }
 
     public static void imprimirFicheroConsola(String ruta){
@@ -29,6 +30,19 @@ public class Main {
     //Creamos el proceso. Ejecutamos el programa que le indiquemos por parámetros
     public static void crearProceso(String nombreApp){
         ProcessBuilder processBuilder = new ProcessBuilder(nombreApp, "ls");
+        try{
+            processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            Process process = processBuilder.start();
+            process.waitFor();
+            System.out.println("Fin del proceso");
+        }catch(IOException | InterruptedException e){
+            System.out.println("Error al lanzar el proceso");
+            e.printStackTrace();
+        }
+    }
+
+    public static void procesoPing(String proceso){
+        ProcessBuilder processBuilder = new ProcessBuilder(proceso, "www.google.com");
         try{
             processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
             Process process = processBuilder.start();
